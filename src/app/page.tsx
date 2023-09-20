8564e601e06e96b4a3ca15ea587fdbaef32c693b
Aaron Hsu <aaron.hsu@duke.edu>
2023-09-20 00:30:17 -0400

    removed framer-motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 import Image from 'next/image'
-import { motion } from 'framer-motion';
 
 export default function Home() {
   return (
     <>
-
-      <h1.motion>
-        aiwfuyuawftyawnftyau
-      </h1.motion>
+      aifulthawiltahwitaf
     </>
   )
 }
