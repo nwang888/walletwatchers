4ad243f327c3cd8e84d9ad5efb30c40004e9a43f
Christian Okokhere <christian.okokhere@duke.edu>
2023-09-25 21:11:03 -0400

    aut template

---
+++ auth.js
@@ -0,0 +1,255 @@
+'use strict';
+
+// read env vars from .env file
+require('dotenv').config();
+const { Configuration, PlaidApi, Products, PlaidEnvironments} = require('plaid');
+const util = require('util');
+const { v4: uuidv4 } = require('uuid');
+const express = require('express');
+const bodyParser = require('body-parser');
+const moment = require('moment');
+const cors = require('cors');
+
+const APP_PORT = process.env.APP_PORT || 8000;
+const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
+const PLAID_SECRET = process.env.PLAID_SECRET;
+const PLAID_ENV = process.env.PLAID_ENV || 'sandbox';
+
+// PLAID_PRODUCTS is a comma-separated list of products to use when initializing
+// Link. Note that this list must contain 'assets' in order for the app to be
+// able to create and retrieve asset reports.
+const PLAID_PRODUCTS = (process.env.PLAID_PRODUCTS || Products.Transactions).split(
+  ',',
+);
+
+// PLAID_COUNTRY_CODES is a comma-separated list of countries for which users
+// will be able to select institutions from.
+const PLAID_COUNTRY_CODES = (process.env.PLAID_COUNTRY_CODES || 'US').split(
+  ',',
+);
+
+// Parameters used for the OAuth redirect Link flow.
+//
+// Set PLAID_REDIRECT_URI to 'http://localhost:3000'
+// The OAuth redirect flow requires an endpoint on the developer's website
+// that the bank website should redirect to. You will need to configure
+// this redirect URI for your client ID through the Plaid developer dashboard
+// at https://dashboard.plaid.com/team/api.
+const PLAID_REDIRECT_URI = process.env.PLAID_REDIRECT_URI || '';
+
+// Parameter used for OAuth in Android. This should be the package name of your app,
+// e.g. com.plaid.linksample
+const PLAID_ANDROID_PACKAGE_NAME = process.env.PLAID_ANDROID_PACKAGE_NAME || '';
+
+// We store the access_token in memory - in production, store it in a secure
+// persistent data store
+let ACCESS_TOKEN = null;
+let PUBLIC_TOKEN = null;
+let ITEM_ID = null;
+// The payment_id is only relevant for the UK/EU Payment Initiation product.
+// We store the payment_id in memory - in production, store it in a secure
+// persistent data store along with the Payment metadata, such as userId .
+let PAYMENT_ID = null;
+// The transfer_id is only relevant for Transfer ACH product.
+// We store the transfer_id in memory - in production, store it in a secure
+// persistent data store
+let TRANSFER_ID = null;
+
+// Initialize the Plaid client
+// Find your API keys in the Dashboard (https://dashboard.plaid.com/account/keys)
+
+const configuration = new Configuration({
+  basePath: PlaidEnvironments[PLAID_ENV],
+  baseOptions: {
+    headers: {
+      'PLAID-CLIENT-ID': PLAID_CLIENT_ID,
+      'PLAID-SECRET': PLAID_SECRET,
+      'Plaid-Version': '2020-09-14',
+    },
+  },
+});
+
+const client = new PlaidApi(configuration);
+
+const app = express();
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  }),
+);
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/api/info', function (request, response, next) {
+  response.json({
+    item_id: ITEM_ID,
+    access_token: ACCESS_TOKEN,
+    products: PLAID_PRODUCTS,
+  });
+});
+
+// Create a link token with configs which we can then use to initialize Plaid Link client-side.
+// See https://plaid.com/docs/#create-link-token
+app.post('/api/create_link_token', function (request, response, next) {
+  Promise.resolve()
+    .then(async function () {
+      const configs = {
+        user: {
+          // This should correspond to a unique id for the current user.
+          client_user_id: 'user-id',
+        },
+        client_name: 'Plaid Quickstart',
+        products: PLAID_PRODUCTS,
+        country_codes: PLAID_COUNTRY_CODES,
+        language: 'en',
+      };
+
+      if (PLAID_REDIRECT_URI !== '') {
+        configs.redirect_uri = PLAID_REDIRECT_URI;
+      }
+
+      if (PLAID_ANDROID_PACKAGE_NAME !== '') {
+        configs.android_package_name = PLAID_ANDROID_PACKAGE_NAME;
+      }
+      const createTokenResponse = await client.linkTokenCreate(configs);
+      prettyPrintResponse(createTokenResponse);
+      response.json(createTokenResponse.data);
+    })
+    .catch(next);
+});
+
+// Create a link token with configs which we can then use to initialize Plaid Link client-side
+// for a 'payment-initiation' flow.
+// See:
+// - https://plaid.com/docs/payment-initiation/
+// - https://plaid.com/docs/#payment-initiation-create-link-token-request
+app.post(
+  '/api/create_link_token_for_payment',
+  function (request, response, next) {
+    Promise.resolve()
+      .then(async function () {
+        const createRecipientResponse =
+          await client.paymentInitiationRecipientCreate({
+            name: 'Harry Potter',
+            iban: 'GB33BUKB20201555555555',
+            address: {
+              street: ['4 Privet Drive'],
+              city: 'Little Whinging',
+              postal_code: '11111',
+              country: 'GB',
+            },
+          });
+        const recipientId = createRecipientResponse.data.recipient_id;
+        prettyPrintResponse(createRecipientResponse);
+
+        const createPaymentResponse =
+          await client.paymentInitiationPaymentCreate({
+            recipient_id: recipientId,
+            reference: 'paymentRef',
+            amount: {
+              value: 1.23,
+              currency: 'GBP',
+            },
+          });
+        prettyPrintResponse(createPaymentResponse);
+        const paymentId = createPaymentResponse.data.payment_id;
+
+        // We store the payment_id in memory for demo purposes - in production, store it in a secure
+        // persistent data store along with the Payment metadata, such as userId.
+        PAYMENT_ID = paymentId;
+
+        const configs = {
+          client_name: 'Plaid Quickstart',
+          user: {
+            // This should correspond to a unique id for the current user.
+            // Typically, this will be a user ID number from your application.
+            // Personally identifiable information, such as an email address or phone number, should not be used here.
+            client_user_id: uuidv4(),
+          },
+          // Institutions from all listed countries will be shown.
+          country_codes: PLAID_COUNTRY_CODES,
+          language: 'en',
+          // The 'payment_initiation' product has to be the only element in the 'products' list.
+          products: [Products.PaymentInitiation],
+          payment_initiation: {
+            payment_id: paymentId,
+          },
+        };
+        if (PLAID_REDIRECT_URI !== '') {
+          configs.redirect_uri = PLAID_REDIRECT_URI;
+        }
+        const createTokenResponse = await client.linkTokenCreate(configs);
+        prettyPrintResponse(createTokenResponse);
+        response.json(createTokenResponse.data);
+      })
+      .catch(next);
+  },
+);
+
+// Exchange token flow - exchange a Link public_token for
+// an API access_token
+// https://plaid.com/docs/#exchange-token-flow
+app.post('/api/set_access_token', function (request, response, next) {
+  PUBLIC_TOKEN = request.body.public_token;
+  Promise.resolve()
+    .then(async function () {
+      const tokenResponse = await client.itemPublicTokenExchange({
+        public_token: PUBLIC_TOKEN,
+      });
+      prettyPrintResponse(tokenResponse);
+      ACCESS_TOKEN = tokenResponse.data.access_token;
+      ITEM_ID = tokenResponse.data.item_id;
+      if (PLAID_PRODUCTS.includes(Products.Transfer)) {
+        TRANSFER_ID = await authorizeAndCreateTransfer(ACCESS_TOKEN);
+      }
+      response.json({
+        // the 'access_token' is a private token, DO NOT pass this token to the frontend in your production environment
+        access_token: ACCESS_TOKEN,
+        item_id: ITEM_ID,
+        error: null,
+      });
+    })
+    .catch(next);
+});
+
+// Retrieve Transactions for an Item
+// https://plaid.com/docs/#transactions
+app.get('/api/transactions', function (request, response, next) {
+  Promise.resolve()
+    .then(async function () {
+      // Set cursor to empty to receive all historical updates
+      let cursor = null;
+
+      // New transaction updates since "cursor"
+      let added = [];
+      let modified = [];
+      // Removed transaction ids
+      let removed = [];
+      let hasMore = true;
+      // Iterate through each page of new transaction updates for item
+      while (hasMore) {
+        const request = {
+          access_token: ACCESS_TOKEN,
+          cursor: cursor,
+        };
+        const response = await client.transactionsSync(request)
+        const data = response.data;
+        // Add this page of results
+        added = added.concat(data.added);
+        modified = modified.concat(data.modified);
+        removed = removed.concat(data.removed);
+        hasMore = data.has_more;
+        // Update cursor to the next cursor
+        cursor = data.next_cursor;
+        prettyPrintResponse(response);
+      }
+
+      const compareTxnsByDateAscending = (a, b) => (a.date > b.date) - (a.date < b.date);
+      // Return the 8 most recent transactions
+      const recently_added = [...added].sort(compareTxnsByDateAscending).slice(-8);
+      response.json({latest_transactions: recently_added});
+    })
+    .catch(next);
+});
+
+
