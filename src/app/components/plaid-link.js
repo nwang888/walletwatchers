982c8d1b0d79e3946a109440f7cb06c799f875f6
Aaron Hsu <aaron.hsu@duke.edu>
2023-10-05 23:22:27 -0400

    fixed all conflicts

--- src/app/components/plaid-link.js
+++ src/app/components/plaid-link.js
@@ -58,30 +58,21 @@ export default function PlaidLink() {
 		setLoading(false);
 	}, [setAccountData, setLoading]);
 
-    return (
-        <div>
-            
-            <div>
-                <button onClick={() => open()} disabled={!ready}>
-                    Link Account
-                </button>
-            </div>
-    
-            {
-                !loading && accountData && 
-                Object.entries(accountData).map((entry, i) => (
-                    <div key={i}>
-                        {JSON.stringify(entry[1], null, 2)}
-                    </div>
-                ))
-            }
+	return (
+		<div>
+			<div>
+				<button onClick={() => open()} disabled={!ready}>
+					Link Account
+				</button>
+			</div>
 
-            <button onClick={() => console.log(accountData)}>log account data</button>
+			{!loading &&
+				accountData &&
+				Object.entries(accountData).map((entry, i) => (
+					<div key={i}>{JSON.stringify(entry[1], null, 2)}</div>
+				))}
 
-        </div>
-
-
-    );
-
-    
+			<button onClick={() => console.log(accountData)}>log account data</button>
+		</div>
+	);
 }
--- src/app/page.js
+++ src/app/page.js
@@ -16,6 +16,7 @@ export default function Home() {
 			<PlaidLink />
 			<WishList />
 			<TransactionDisplay />
+			<Budget />
 		</>
 	);
 }
