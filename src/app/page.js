a7cc7f0448f0539ded78b58c6c7bfc9f4d1391b4
nwang888 <nwang888@gmail.com>
2023-10-05 21:42:21 -0400

    front end is connected

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,20 @@
-'use client'
+"use client";
 
-import Image from 'next/image';
-import Button from '@mui/material/Button';
-import { useState } from 'react';
+import Image from "next/image";
+import Button from "@mui/material/Button";
+import { useState } from "react";
 
-import FoodRating from './components/food-rating';
-import PlaidLink from './components/plaid-link';
+import FoodRating from "./components/food-rating";
+import PlaidLink from "./components/plaid-link";
+import TransactionDisplay from "./components/transactions-display";
 
 export default function Home() {
+	const [data, setData] = useState("");
 
-  const [data, setData] = useState("");
-
-  return (
-    <>
-      <PlaidLink />
-    </>
-  )
+	return (
+		<>
+			<PlaidLink />
+			<TransactionDisplay />
+		</>
+	);
 }
