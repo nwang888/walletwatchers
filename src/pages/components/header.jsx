a30f81c6c67ce6cf0658cf5d5eaa09de55687366
Aaron Hsu <aaron.hsu@duke.edu>
2023-11-04 19:16:34 -0400

    framer motion and center navbar

--- src/pages/components/header.jsx
+++ src/pages/components/header.jsx
@@ -1,11 +1,23 @@
 import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 
-export default function NavBar({ setPageNum }) {
+export default function NavBar() {
 
 
   return (
+
     <>
+      <motion.div
+        whileHover={{ x: 1.1 }}
+        transition={{
+            type: "spring",
+            duration: 0.3
+        }}
+      >
+
         <h1>Walletwatchers</h1>
+      </motion.div>
+
     </>
   )
 }
--- src/pages/components/navigation-bar.jsx
+++ src/pages/components/navigation-bar.jsx
@@ -1,24 +1,55 @@
 import { useEffect, useState } from 'react';
 import { Flex, Button } from '@radix-ui/themes';
 
+import { motion } from 'framer-motion';
+
 export default function NavBar({ setPageNum }) {
 
 
   return (
     <>
-      <Flex direction="row" gap="3">
-        <Button onClick={() => setPageNum(0)}>
-            Home
-        </Button>
+      <div className='fixed inset-x-0 bottom-0 flex justify-center items-center bg-white py-3'>
+        <Flex direction="row" gap="3">
+          <motion.div
+            className="mx-[10vw]"
+            whileHover={{ scale: 1.05 }}
+            transition={{
+                type: "spring",
+                duration: 0.3
+            }}
+          >
+            <Button onClick={() => setPageNum(0)}>
+                Home
+            </Button>
+          </motion.div>
 
-        <Button onClick={() => setPageNum(1)}>
-            Transactions
-        </Button>
+          <motion.div
+            className="mx-[10vw]"
+            whileHover={{ scale: 1.05 }}
+            transition={{
+                type: "spring",
+                duration: 0.3
+            }}
+          >
+            <Button onClick={() => setPageNum(1)}>
+                Transactions
+            </Button>
+          </motion.div>
 
-        <Button onClick={() => setPageNum(2)}>
-            Wishlist
-        </Button>
-      </Flex>
+          <motion.div
+            className="mx-[10vw]"
+            whileHover={{ scale: 1.05 }}
+            transition={{
+                type: "spring",
+                duration: 0.3
+            }}
+          >
+            <Button onClick={() => setPageNum(2)}>
+                Wishlist
+            </Button>
+          </motion.div>
+        </Flex>
+      </div>
     </>
   )
 }
