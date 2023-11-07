7853953266c3bbae8f25d953786ba80828d43e0e
nwang888 <nwang888@gmail.com>
2023-11-06 21:50:09 -0500

    added pagination

--- src/pages/components/transactions-table.jsx
+++ src/pages/components/transactions-table.jsx
@@ -6,11 +6,12 @@ import { motion } from "framer-motion";
 //TODO: Add pagination
 //TODO: Add search
 //TODO: Add filtering for all columns
-//TODO: Add date range
-//TODO: Componentize table
 
 export default function TransactionsTable() {
 	const [transactions, setTransactions] = useState([]);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [rowsPerPage, setRowsPerPage] = useState(10);
+	const [sortAttribute, setSortAttribute] = useState("datetime");
 	const [sortOrder, setSortOrder] = useState({
 		category_primary: "asc",
 		category_detailed: "asc",
@@ -22,18 +23,32 @@ export default function TransactionsTable() {
 		account_name: "asc"
 	});
 
+	const handlePageChange = (newPage) => {
+		setCurrentPage(newPage);
+		getTransactionsData(
+			sortAttribute,
+			sortOrder[sortAttribute],
+			newPage,
+			rowsPerPage
+		);
+	};
+
 	const handleSort = (attribute) => {
 		const newOrder = sortOrder[attribute] === "asc" ? "desc" : "asc";
+		setSortAttribute(attribute);
 		setSortOrder({ ...sortOrder, [attribute]: newOrder });
 		getTransactionsData(attribute, newOrder);
 	};
 
 	const getTransactionsData = async (
 		sort_by = "transaction_amount",
-		order = "desc"
+		order = "desc",
+		page = 1,
+		rowsPerPage = 10,
+		paginate = true
 	) => {
 		const response = await fetch(
-			`/api/transactions?sort_by=${sort_by}&order=${order}`
+			`/api/transactions?sort_by=${sort_by}&order=${order}&page=${page}&rowsPerPage=${rowsPerPage}&paginate=${paginate}`
 		);
 		const data = await response.json();
 		setTransactions(data);
@@ -46,173 +61,184 @@ export default function TransactionsTable() {
 	return (
 		<div>
 			{transactions.length > 0 ? (
-				<Table.Root variant="surface">
-					<Table.Header>
-						<Table.Row>
-							<Table.ColumnHeaderCell>
-								<div style={{ display: "flex", alignItems: "center" }}>
-									Account
-									<Flex gap="3">
-										<Button
-											radius="large"
-											variant="surface"
-											highContrast
-											color="orange"
-											size="1"
-											onClick={() => handleSort("account_name")}
-											style={{ marginLeft: "5px" }}
-										>
-											Sort
-										</Button>
-									</Flex>
-								</div>
-							</Table.ColumnHeaderCell>
-							<Table.ColumnHeaderCell>
-								<div style={{ display: "flex", alignItems: "center" }}>
-									Primary Category
-									<Flex gap="3">
-										<Button
-											radius="large"
-											variant="surface"
-											highContrast
-											color="orange"
-											size="1"
-											onClick={() => handleSort("category_primary")}
-											style={{ marginLeft: "5px" }}
-										>
-											Sort
-										</Button>
-									</Flex>
-								</div>
-							</Table.ColumnHeaderCell>
-							<Table.ColumnHeaderCell>
-								<div style={{ display: "flex", alignItems: "center" }}>
-									Detailed Category
-									<Flex gap="3">
-										<Button
-											radius="large"
-											variant="surface"
-											highContrast
-											color="orange"
-											size="1"
-											onClick={() => handleSort("category_detailed")}
-											style={{ marginLeft: "5px" }}
-										>
-											Sort
-										</Button>
-									</Flex>
-								</div>
-							</Table.ColumnHeaderCell>
-							<Table.ColumnHeaderCell>
-								<div style={{ display: "flex", alignItems: "center" }}>
-									Merchant Name
-									<Flex gap="3">
-										<Button
-											radius="large"
-											variant="surface"
-											highContrast
-											color="orange"
-											size="1"
-											onClick={() => handleSort("merchant_name")}
-											style={{ marginLeft: "5px" }}
-										>
-											Sort
-										</Button>
-									</Flex>
-								</div>
-							</Table.ColumnHeaderCell>
-							<Table.ColumnHeaderCell>
-								<div style={{ display: "flex", alignItems: "center" }}>
-									Transaction Amount
-									<Flex gap="3">
-										<Button
-											radius="large"
-											variant="surface"
-											highContrast
-											color="orange"
-											size="1"
-											onClick={() => handleSort("transaction_amount")}
-											style={{ marginLeft: "5px" }}
-										>
-											Sort
-										</Button>
-									</Flex>
-								</div>
-							</Table.ColumnHeaderCell>
-							<Table.ColumnHeaderCell>
-								<div style={{ display: "flex", alignItems: "center" }}>
-									City
-									<Flex gap="3">
-										<Button
-											radius="large"
-											variant="surface"
-											highContrast
-											color="orange"
-											size="1"
-											onClick={() => handleSort("city")}
-											style={{ marginLeft: "5px" }}
-										>
-											Sort
-										</Button>
-									</Flex>
-								</div>
-							</Table.ColumnHeaderCell>
-							<Table.ColumnHeaderCell>
-								<div style={{ display: "flex", alignItems: "center" }}>
-									Region
-									<Flex gap="3">
-										<Button
-											radius="large"
-											variant="surface"
-											highContrast
-											color="orange"
-											size="1"
-											onClick={() => handleSort("region")}
-											style={{ marginLeft: "5px" }}
-										>
-											Sort
-										</Button>
-									</Flex>
-								</div>
-							</Table.ColumnHeaderCell>
-							<Table.ColumnHeaderCell>
-								<div style={{ display: "flex", alignItems: "center" }}>
-									Datetime
-									<Flex gap="3">
-										<Button
-											radius="large"
-											variant="surface"
-											highContrast
-											color="orange"
-											size="1"
-											onClick={() => handleSort("datetime")}
-											style={{ marginLeft: "5px" }}
-										>
-											Sort
-										</Button>
-									</Flex>
-								</div>
-							</Table.ColumnHeaderCell>
-						</Table.Row>
-					</Table.Header>
-
-					<Table.Body>
-						{transactions.map((transaction, index) => (
-							<Table.Row key={index}>
-								<Table.RowHeaderCell>
-									{transaction.account_name}
-								</Table.RowHeaderCell>
-								<Table.Cell>{transaction.category_primary}</Table.Cell>
-								<Table.Cell>{transaction.category_detailed}</Table.Cell>
-								<Table.Cell>{transaction.merchant_name}</Table.Cell>
-								<Table.Cell>{transaction.transaction_amount}</Table.Cell>
-								<Table.Cell>{transaction.city}</Table.Cell>
-								<Table.Cell>{transaction.region}</Table.Cell>
-								<Table.Cell>{transaction.datetime}</Table.Cell>
+				<div>
+					<Table.Root variant="surface">
+						<Table.Header>
+							<Table.Row>
+								<Table.ColumnHeaderCell>
+									<div style={{ display: "flex", alignItems: "center" }}>
+										Account
+										<Flex gap="3">
+											<Button
+												radius="large"
+												variant="surface"
+												highContrast
+												color="orange"
+												size="1"
+												onClick={() => handleSort("account_name")}
+												style={{ marginLeft: "5px" }}
+											>
+												Sort
+											</Button>
+										</Flex>
+									</div>
+								</Table.ColumnHeaderCell>
+								<Table.ColumnHeaderCell>
+									<div style={{ display: "flex", alignItems: "center" }}>
+										Primary Category
+										<Flex gap="3">
+											<Button
+												radius="large"
+												variant="surface"
+												highContrast
+												color="orange"
+												size="1"
+												onClick={() => handleSort("category_primary")}
+												style={{ marginLeft: "5px" }}
+											>
+												Sort
+											</Button>
+										</Flex>
+									</div>
+								</Table.ColumnHeaderCell>
+								<Table.ColumnHeaderCell>
+									<div style={{ display: "flex", alignItems: "center" }}>
+										Detailed Category
+										<Flex gap="3">
+											<Button
+												radius="large"
+												variant="surface"
+												highContrast
+												color="orange"
+												size="1"
+												onClick={() => handleSort("category_detailed")}
+												style={{ marginLeft: "5px" }}
+											>
+												Sort
+											</Button>
+										</Flex>
+									</div>
+								</Table.ColumnHeaderCell>
+								<Table.ColumnHeaderCell>
+									<div style={{ display: "flex", alignItems: "center" }}>
+										Merchant Name
+										<Flex gap="3">
+											<Button
+												radius="large"
+												variant="surface"
+												highContrast
+												color="orange"
+												size="1"
+												onClick={() => handleSort("merchant_name")}
+												style={{ marginLeft: "5px" }}
+											>
+												Sort
+											</Button>
+										</Flex>
+									</div>
+								</Table.ColumnHeaderCell>
+								<Table.ColumnHeaderCell>
+									<div style={{ display: "flex", alignItems: "center" }}>
+										Transaction Amount
+										<Flex gap="3">
+											<Button
+												radius="large"
+												variant="surface"
+												highContrast
+												color="orange"
+												size="1"
+												onClick={() => handleSort("transaction_amount")}
+												style={{ marginLeft: "5px" }}
+											>
+												Sort
+											</Button>
+										</Flex>
+									</div>
+								</Table.ColumnHeaderCell>
+								<Table.ColumnHeaderCell>
+									<div style={{ display: "flex", alignItems: "center" }}>
+										City
+										<Flex gap="3">
+											<Button
+												radius="large"
+												variant="surface"
+												highContrast
+												color="orange"
+												size="1"
+												onClick={() => handleSort("city")}
+												style={{ marginLeft: "5px" }}
+											>
+												Sort
+											</Button>
+										</Flex>
+									</div>
+								</Table.ColumnHeaderCell>
+								<Table.ColumnHeaderCell>
+									<div style={{ display: "flex", alignItems: "center" }}>
+										Region
+										<Flex gap="3">
+											<Button
+												radius="large"
+												variant="surface"
+												highContrast
+												color="orange"
+												size="1"
+												onClick={() => handleSort("region")}
+												style={{ marginLeft: "5px" }}
+											>
+												Sort
+											</Button>
+										</Flex>
+									</div>
+								</Table.ColumnHeaderCell>
+								<Table.ColumnHeaderCell>
+									<div style={{ display: "flex", alignItems: "center" }}>
+										Datetime
+										<Flex gap="3">
+											<Button
+												radius="large"
+												variant="surface"
+												highContrast
+												color="orange"
+												size="1"
+												onClick={() => handleSort("datetime")}
+												style={{ marginLeft: "5px" }}
+											>
+												Sort
+											</Button>
+										</Flex>
+									</div>
+								</Table.ColumnHeaderCell>
 							</Table.Row>
-						))}
-					</Table.Body>
-				</Table.Root>
+						</Table.Header>
+
+						<Table.Body>
+							{transactions.map((transaction, index) => (
+								<Table.Row key={index}>
+									<Table.RowHeaderCell>
+										{transaction.account_name}
+									</Table.RowHeaderCell>
+									<Table.Cell>{transaction.category_primary}</Table.Cell>
+									<Table.Cell>{transaction.category_detailed}</Table.Cell>
+									<Table.Cell>{transaction.merchant_name}</Table.Cell>
+									<Table.Cell>{transaction.transaction_amount}</Table.Cell>
+									<Table.Cell>{transaction.city}</Table.Cell>
+									<Table.Cell>{transaction.region}</Table.Cell>
+									<Table.Cell>{transaction.datetime}</Table.Cell>
+								</Table.Row>
+							))}
+						</Table.Body>
+					</Table.Root>
+					<button
+						onClick={() => handlePageChange(currentPage - 1)}
+						disabled={currentPage === 1}
+					>
+						Previous
+					</button>
+					<button onClick={() => handlePageChange(currentPage + 1)}>
+						Next
+					</button>
+				</div>
 			) : (
 				<p>No transactions found.</p>
 			)}
