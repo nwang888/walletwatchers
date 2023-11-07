438e6b3456b91b8a003614a48b20400aa8eb536c
nwang888 <nwang888@gmail.com>
2023-11-06 21:28:15 -0500

    test

--- src/pages/components/header.jsx
+++ src/pages/components/header.jsx
@@ -1,20 +1,23 @@
-import { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
-export default function NavBar({ setPageNum }) {
-  return (
-    <div className="fixed inset-x-0 top-0 flex justify-center items-center py-3">
-      <motion.div
-        whileHover={{ scale: 1.1 }}
-        transition={{
-            type: "spring",
-            duration: 0.3
-        }}
-      >
-
-        <h1 className="text-xl font-bold font-header" onClick={() => setPageNum(0)}>WALLETWATCHERS</h1>
-
-      </motion.div>
-    </div>
-  );
+export default function Header({ setPageNum }) {
+	return (
+		<div className="fixed inset-x-0 top-0 flex justify-center items-center py-3 bg-custom-purple">
+			<motion.div
+				whileHover={{ scale: 1.1 }}
+				transition={{
+					type: "spring",
+					duration: 0.3
+				}}
+			>
+				<h1
+					className="text-xl font-bold font-header"
+					onClick={() => setPageNum(0)}
+				>
+					WALLETWATCHERS
+				</h1>
+			</motion.div>
+		</div>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        'sans': ['Inter', 'sans-serif'],
-        'header': ['Plus Jakarta Sans', 'sans-serif'],
-      }
-    }
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				"sans": ["Inter", "sans-serif"],
+				"header": ["Plus Jakarta Sans", "sans-serif"]
+			},
+			colors: {
+				"custom-purple": "#AA87BE"
+			}
+		}
+	},
+	plugins: []
+};
