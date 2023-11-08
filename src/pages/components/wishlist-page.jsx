9b2d064d83f1c6d6c3e8c9799e435f9150c18186
Hannah Choi <hyc8@duke.edu>
2023-11-07 22:01:27 -0500

    edits to table

--- src/pages/components/wishlist-page.jsx
+++ src/pages/components/wishlist-page.jsx
@@ -1,3 +1,4 @@
+
 import React, { useEffect, useState, useRef} from 'react';
 // import * as radix from '@radix-ui';
 // import 'node_modules/@radix-ui/react-progress/dist/index.css';
@@ -9,6 +10,9 @@ import Image from 'next/image';
 // import Rating from '@mui/material/Rating';
 // import { TextInput } from '@radix-ui/react-text-input';
 import { Table } from '@radix-ui/themes';
+import * as Progress from '@radix-ui/react-progress'
+// import './styles.css';
+ 
 
 
 export  async function handler(req, res) {
@@ -30,9 +34,11 @@ export  async function handler(req, res) {
 }
 
 export default function WishlistPage({ balance }) {
-
     const [nameTextBox, setNameTextBox] = useState("");
     const [priceTextBox, setPriceTextBox] = useState(0);
+    const [newItem, setNewItem] = useState({ name: '', price: '' });
+    const [currentPage, setCurrentPage] = useState(1);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
 
     const [name, setName] = useState([]);
     const [price, setPrice] = useState([]);
@@ -45,6 +51,25 @@ export default function WishlistPage({ balance }) {
   //         .then(response => response.json())
   //         .then(data => setTotalBalance(data.account_balance));
   // }, []);
+    	const handleRowsPerPageChange = (event) => {
+        setRowsPerPage(parseInt(event.target.value));
+        getWishlistData(
+          currentPage,
+          parseInt(event.target.value),
+          paginate = true
+        );
+        page = 1,
+		rowsPerPage = 10,
+		paginate = true
+      };
+      
+    const handlePageChange = (newPage) => {
+      setCurrentPage(newPage);
+      getWishlistsData(
+        newPage,
+        rowsPerPage
+      );
+    };
 
     useEffect(() => {
         fetch('/api/account') // Assuming '/api/wishlist' returns your data
@@ -67,8 +92,8 @@ export default function WishlistPage({ balance }) {
     }, []);
   
  
-    const getWishlistData = async () => {
-        const response = await fetch('/api/wishlist');
+    const getWishlistData = async ( page=1, rowsPerPage = 10 , paginate = true) => {
+        const response = await fetch(`/api/wishlist?page=${page}&rowsPerPage=${rowsPerPage}&paginate=${paginate}`);
         const payload = await response.json();
     
         let newId = [];
@@ -81,10 +106,10 @@ export default function WishlistPage({ balance }) {
             newPrice.push(payload[i].item_price);
         }
     
-        setID(newId);
+        setID(newId); 
         setName(newName);
         setPrice(newPrice);
-        
+        setWishlist([...wishlist, { name: 'Manual Name', price: 'Manual Price' }]);
     }
     
     const postWishlistData = async () => {
@@ -92,6 +117,8 @@ export default function WishlistPage({ balance }) {
             name: nameTextBox,
             price: priceTextBox
         };
+        getWishlistData();
+        setWishlist([...wishlist, {name: "hi", price:" 5"}]);
         
         console.log("sending data: ", dataToSend);
 
@@ -108,36 +135,41 @@ export default function WishlistPage({ balance }) {
         const data = await res.json();
         // console.log('Response (from client):', data);
         // console.log(priceTextBox);
+        getWishlistData();
+        setWishlist([...wishlist, {name: "hi", price:" 5"}]);
     }  
 
   useEffect(() => {
     getWishlistData();
+ 
+    //fix: https://stackoverflow.com/questions/72153376/multiple-charts-in-one-page-chartjs 
+  //   const ctx = document.getElementById('progressBar').getContext('2d');
 
-    const ctx = document.getElementById('progressBar').getContext('2d');
     
-    const progressBar = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ['Progress'],
-        datasets: [{
-          label: 'Progress',
-          data: [75], 
-          // data: [price],
-          backgroundColor: 'rgba(75, 192, 192, 0.2)',
-          borderColor: 'rgb(75, 192, 192)',
-          borderWidth: 1
-        }]
-      }, 
-      options: {
-        indexAxis: 'y',
-        scales: { 
-          y: {
-            beginAtZero: true,
-            max: 100 // This represents 100%
-          }
-        }
-      }
-     });
+    
+  //   const progressBar = new Chart(ctx, {
+  //     type: 'bar',
+  //     data: {
+  //       labels: ['Progress'],
+  //       datasets: [{
+  //         label: 'Progress',
+  //         data: [totalBalance], 
+  //         // data: [price],
+  //         backgroundColor: 'rgba(75, 192, 192, 0.2)',
+  //         borderColor: 'rgb(75, 192, 192)',
+  //         borderWidth: 1
+  //       }] 
+  //     }, 
+  //     options: {
+  //       indexAxis: 'y',
+  //       scales: { 
+  //         y: {
+  //           beginAtZero: true,
+  //           max: totalPrice // This represents 100%
+  //         }
+  //       }
+  //     }
+  //    });
 
      
 
@@ -160,6 +192,8 @@ export default function WishlistPage({ balance }) {
     
 const [wishlist, setWishlist] = useState([]);
 
+
+
 useEffect(() => {
     fetch('/api/wishlist') // Assuming '/api/wishlist' returns your data
         .then(response => response.json())
@@ -169,16 +203,12 @@ useEffect(() => {
   return (
     
     <>
-    {/* <h1>hi</h1>
-    <Progress.Root value={progress}>
-       <Progress.Indicator />
-     </Progress.Root> */}
 
    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
 
       
       <h1>Wishlist</h1>
-        <p>there's data stored in state that isn't rendered yet</p>
+        
     
      
       <div>
@@ -188,7 +218,7 @@ useEffect(() => {
               type="text"  
               label = "Name" 
               value= {nameTextBox}
-              onChange= {(event) => setNameTextBox(event.target.value)}
+              onChange= {(e) => setNameTextBox(e.target.value)}
           />    
           <input  
                   id="outlined-basic" 
@@ -197,66 +227,89 @@ useEffect(() => {
                   value= {priceTextBox} 
                   onChange= {(event) => setPriceTextBox(event.target.value)} 
               /> 
-                      <button onClick={getWishlistData}>get wishlist</button>
+                      {/* <button onClick={getWishlistData}> Update </button> */}
+                      <button onClick={postWishlistData}> Add </button>
     
-              <button id = "submit-button" onClick={postWishlistData}>Submit</button>
-              <h1> {totalBalance} </h1> 
-              <h1> {totalPrice} </h1> 
-      </div>
+             
+      </div> 
 
 
 
 
 
           <div>
-			{wishlist.length > 0 ? (
-				<Table.Root variant="surface">
-					<Table.Header>
-						<Table.Row>
-							<Table.ColumnHeaderCell>
-								<div style={{ display: "flex", alignItems: "center" }}>
-									Account
-								</div>
-							</Table.ColumnHeaderCell>
-							<Table.ColumnHeaderCell>
-								<div style={{ display: "flex", alignItems: "center" }}>
-									Name
-								</div>
-							</Table.ColumnHeaderCell>
-							<Table.ColumnHeaderCell>
-								<div style={{ display: "flex", alignItems: "center" }}>
-									Price
-								</div>
-							</Table.ColumnHeaderCell>
-              {/* <Table.ColumnHeaderCell>
-								<div style={{ display: "flex", alignItems: "center" }}>
-									Name
-								</div>
-							</Table.ColumnHeaderCell> */}
-						</Table.Row>
-					</Table.Header> 
+      {wishlist.length > 0 ? (
+        <Table.Root variant="surface">
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeaderCell>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  Number
+                </div>
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  Name
+                </div>
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  Price
+                </div>
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  Progress
+                </div>
+              </Table.ColumnHeaderCell>
+            </Table.Row>
+          </Table.Header> 
 
-					<Table.Body>  
-						{id.map((item, idx) => (
-							<Table.Row key={id[idx]}>
-								<Table.RowHeaderCell>
-									{id[idx]} 
-								</Table.RowHeaderCell>
-								<Table.Cell>{name[idx]}</Table.Cell>
-								<Table.Cell>{price[idx]}</Table.Cell>
-                <Table.Cell> </Table.Cell>
-							</Table.Row>
-						))} 
-					</Table.Body>
-				</Table.Root>
+          <Table.Body>  
+            {id.map((item, idx) => (
+              <Table.Row key={id[idx]}>
+                <Table.RowHeaderCell>
+                  {id[idx]} 
+                </Table.RowHeaderCell>
+                <Table.Cell>{name[idx]}</Table.Cell>
+                <Table.Cell>{price[idx]}</Table.Cell>
+                <Table.Cell> <progress value={totalBalance} max={price[idx]} /> </Table.Cell>
+              </Table.Row>
+            ))} 
+          </Table.Body>
+        </Table.Root>
 
-        
-			) : (
-				<p>No transactions found.</p>
-			)}
+         
+      ) : (
+        <p>No transactions found.</p>
+      )} 
 
-      <canvas id="progressBar"></canvas>  
-		</div>
+<label>
+						Rows per page:
+						<select value={rowsPerPage} onChange={handleRowsPerPageChange}>
+							<option value={10}>10</option>
+							<option value={20}>20</option>
+							<option value={50}>50</option>
+						</select>
+					</label>
+					<button
+						onClick={() => handlePageChange(currentPage - 1)}
+						disabled={currentPage === 1}
+					>
+						Previous
+					</button>
+					<button onClick={() => handlePageChange(currentPage + 1)}>
+						Next
+					</button>
+
+ 
+          
+
+    </div>
+
+    {/* <canvas id="progressBar"></canvas> */}
+    {/* <canvas id="progressBar"></canvas> */}
           </>
   );
 };
+//       return an error if 
