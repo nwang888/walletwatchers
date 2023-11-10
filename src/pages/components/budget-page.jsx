04e62a1c955fcbd82f195e6794bd6ee1e1f7dbf7
Christian Okokhere <christian.okokhere@duke.edu>
2023-11-09 19:38:59 -0500

    the budgets chart with the dbcalls

--- src/pages/components/budget-page.jsx
+++ src/pages/components/budget-page.jsx
@@ -1,20 +1,91 @@
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import Chart from 'chart.js/auto';
 
-export default function BudgetPage({}) {
+export default function BudgetPage() {
+  const [budgets, setBudgets] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const chartRef = useRef(null);
+  const [chartInstance, setChartInstance] = useState(null);
+
   useEffect(() => {
     const getBudgets = async () => {
-      const response = await fetch('/api/budgets');
-      const payload = await response.json();
-      console.log(payload);
-    }
+      try {
+        const response = await fetch('/api/budgets');
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const payload = await response.json();
+        setBudgets(payload);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Failed to fetch budgets:", error);
+      }
+    };
 
     getBudgets();
   }, []);
 
+  useLayoutEffect(() => {
+    if (isLoading || chartInstance) return;
+    const budgetLabels = budgets.map(budget => budget.budget_name);
+    const budgetAmounts = budgets.map(budget => budget.budget_amount);
+
+    const data = {
+      labels: budgetLabels,
+      datasets: [{
+        label: 'Budget Distribution',
+        data: budgetAmounts,
+        backgroundColor: [
+          '#CCDFF1',
+          '#EDDEF3',
+          '#E8F5E4',
+          '#C4E6DE',
+          '#CFEAF2',
+        ],
+        hoverOffset: 4
+      }]
+    };
+
+    const config = {
+      type: 'bar',
+      data: data,
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Budget Distribution'
+          }
+        }
+      },
+    };
+
+    // Instantiate new Chart
+    const newChartInstance = new Chart(chartRef.current, config);
+    setChartInstance(newChartInstance);
+
+    // Cleanup
+    return () => {
+      if (chartInstance) {
+        chartInstance.destroy();
+      }
+    };
+  }, [isLoading, budgets]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-  <>
-    <h1>Budget</h1>
-      <p>there's data stored in state that isn't rendered yet</p>
-  </>
-)
-};
+    <>
+      <h1>Budget</h1>
+      <div className="w-full p-3 bg-slate-50 rounded-md">
+        <canvas ref={chartRef} id="budgetChart" />
+      </div>
+      {}
+    </>
+  );
+}
