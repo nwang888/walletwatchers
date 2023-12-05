405381135dc5ed238b2d5e7fca2a3eadb77ddf55
Aaron Hsu <aaron.hsu@duke.edu>
2023-12-05 16:31:12 -0500

    map for rendering

--- src/pages/components/landing/info-boxes.jsx
+++ src/pages/components/landing/info-boxes.jsx
@@ -6,6 +6,16 @@ export default function InfoBoxes() {
     const delay = 2;
     const stagger = 0.8;
 
+    const data = {
+        "header": ["Connect your accounts", "Track your spending", "Budget your expenses"],
+        "text": [
+            "Easily link and view the balances of each of your accounts at a glance. Delve deeper into individual account expenses for meticulous financial tracking and improved budget control. ", 
+            "View all your transactions across different accounts in one place and filter them as you need. Experience the ease of tracking your spending and take the guesswork out of managing your finances.", 
+            "Set customized budgets for various spending categories to keep your expenses in check. Prevent overspending and make budgeting simple, personal, and efficient."
+        ],
+        "image": ["images/wallets.png", "images/transactions.png", "images/budgets.png"]
+    }
+
     return (
         <motion.div
                 className="flex flex-row justify-center items-center"
@@ -15,30 +25,17 @@ export default function InfoBoxes() {
                 transition={{ duration: 0.4, delay: delay }}
               >
 
-                <InfoBox 
-                    header="Connect your accounts"
-                    text="Easily link and view the balances of each of your accounts at a glance. Delve deeper into individual account expenses for meticulous financial tracking and improved budget control. "
-                    image="images/wallets.png"
-                    delay={delay + stagger * 1}
-                
-                />
-
-                <InfoBox 
-                    header="Track your spending"
-                    text="View all your transactions across different accounts in one place and filter them as you need. Experience the ease of tracking your spending and take the guesswork out of managing your finances."
-                    image="images/transactions.png"
-                    delay={delay + stagger * 2}
-                
-                />
-
-
-                <InfoBox 
-                    header="Budget your expenses"
-                    text="Set customized budgets for various spending categories to keep your expenses in check. Prevent overspending and make budgeting simple, personal, and efficient."
-                    image="images/budgets.png"
-                    delay={delay + stagger * 3}
-                
-                />
+                {
+                    data.header.map((header, index) => (
+                        <InfoBox 
+                            header={header}
+                            text={data.text[index]}
+                            image={data.image[index]}
+                            delay={delay + stagger * index}
+                        
+                        />
+                    ))
+                }
 
 
 
