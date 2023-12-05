2882822969170ba5c237c7d99c59a6a183b3de37
Christian Okokhere <christian.okokhere@duke.edu>
2023-12-04 23:03:47 -0500

    added in the recurring transactions route, added in graph, refactored set budget

--- src/pages/components/budget-page.jsx
+++ src/pages/components/budget-page.jsx
@@ -1,19 +1,22 @@
-import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import Chart from 'chart.js/auto';
 
 import RecurringTransactions from './budget/recurring-transactions';
-
+import BudgetForm from './budget/set-budget';
 
 export default function BudgetPage() {
   const [budgets, setBudgets] = useState([]);
+  const [recurringTransactions, setRecurringTransactions] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isRecurringLoading, setIsRecurringLoading] = useState(true);
   const chartRef = useRef(null);
+  const [chartInstance, setChartInstance] = useState(null);
   const [budgetName, setBudgetName] = useState('');
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
   const [budgetAmount, setBudgetAmount] = useState('');
-  const [chartInstance, setChartInstance] = useState(null);
 
+  // Fetch budgets
   useEffect(() => {
     const getBudgets = async () => {
       try {
@@ -29,28 +32,44 @@ export default function BudgetPage() {
         setIsLoading(false);
       }
     };
-
     getBudgets();
   }, []);
 
+  // Fetch recurring transactions
+  useEffect(() => {
+    const getRecurringTransactions = async () => {
+      try {
+        const response = await fetch('/api/recurring-transactions');
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const payload = await response.json();
+        setRecurringTransactions(payload);
+      } catch (error) {
+        console.error("Failed to fetch recurring transactions:", error);
+      } finally {
+        setIsRecurringLoading(false);
+      }
+    };
+    getRecurringTransactions();
+  }, []);
+
+  // Create chart
   useLayoutEffect(() => {
-    if (isLoading || chartInstance || !Array.isArray(budgets)) return;
+    if (isLoading || isRecurringLoading || chartInstance || !Array.isArray(budgets) || !Array.isArray(recurringTransactions)) return;
 
     const budgetLabels = budgets.map(budget => budget.budget_name);
     const budgetAmounts = budgets.map(budget => budget.budget_amount);
+    const recurringAmounts = recurringTransactions.map(transaction => transaction.transaction_amount);
+    const combinedAmounts = budgetAmounts.concat(recurringAmounts);
+    const combinedLabels = budgetLabels.concat(recurringTransactions.map(transaction => transaction.merchant_name));
 
     const data = {
-      labels: budgetLabels,
+      labels: combinedLabels,
       datasets: [{
-        label: 'Budget Distribution',
-        data: budgetAmounts,
-        backgroundColor: [
-          '#CCDFF1',
-          '#EDDEF3',
-          '#E8F5E4',
-          '#C4E6DE',
-          '#CFEAF2',
-        ],
+        label: 'Budget and Recurring Transactions',
+        data: combinedAmounts,
+        backgroundColor: ['#CCDFF1', '#EDDEF3', '#E8F5E4', '#C4E6DE', '#CFEAF2'],
         hoverOffset: 4
       }]
     };
@@ -62,11 +81,11 @@ export default function BudgetPage() {
         responsive: true,
         plugins: {
           legend: {
- position: 'top',
- },
+            position: 'top',
+          },
           title: {
             display: true,
-            text: 'Budget Distribution'
+            text: 'Budget and Recurring Transactions'
           }
         }
       },
@@ -80,38 +99,9 @@ export default function BudgetPage() {
         chartInstance.destroy();
       }
     };
-  }, [isLoading, budgets]);
-  const handleSetBudget = async () => {
-    const budget_id = Math.floor(Math.random() * 1000000); // Random ID for the budget
-    const budget = { budget_id, budget_name: budgetName, start_date: startDate, end_date: endDate, budget_amount: budgetAmount };
+  }, [isLoading, isRecurringLoading, budgets, recurringTransactions, chartInstance]);
 
-    try {
-      const response = await fetch('/api/budgets', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(budget),
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const result = await response.json();
-      alert(result.message); // Alert the success message
-      // Optionally reset form or update UI here
-      setBudgetName('');
-      setStartDate('');
-      setEndDate('');
-      setBudgetAmount('');
-    } catch (error) {
-      console.error("Failed to post budget:", error);
-      alert("Failed to set budget.");
-    }
-  };
-
-
-  if (isLoading) {
+  if (isLoading || isRecurringLoading) {
     return <div>Loading...</div>;
   }
 
@@ -125,51 +115,8 @@ export default function BudgetPage() {
           <div>No budget data to display</div>
         )}
       </div>
-      <div className="flex justify-center my-5">
-        <div className="w-1/2 p-3 bg-slate-50 rounded-md">
-          <h1 className="text-xl mb-4">Set Budget</h1>
-          <div className="flex flex-col">
-            <input
-              className="mb-2 p-2 border rounded"
-              type="text"
-              value={budgetName}
-              onChange={e => setBudgetName(e.target.value)}
-              placeholder="Budget Name"
-            />
-            <input
-              className="mb-2 p-2 border rounded"
-              type="date"
-              value={startDate}
-              onChange={e => setStartDate(e.target.value)}
-              placeholder="Start Date"
-            />
-            <input
-              className="mb-2 p-2 border rounded"
-              type="date"
-              value={endDate}
-              onChange={e => setEndDate(e.target.value)}
-              placeholder="End Date"
-            />
-            <input
-              className="mb-2 p-2 border rounded"
-              type="number"
-              value={budgetAmount}
-              onChange={e => setBudgetAmount(e.target.value)}
-              placeholder="Budget Amount"
-            />
-            <button
-              className="p-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition-colors"
-              onClick={handleSetBudget}
-            >
-              Set Budget
-            </button>
-          </div>
-        </div>
-      </div>
-
-
+      <BudgetForm />
       <RecurringTransactions />
-
     </>
   );
 }
--- src/pages/components/budget/set-budget.jsx
+++ src/pages/components/budget/set-budget.jsx
@@ -1,52 +1,49 @@
 import React, { useState } from 'react';
-import { Table, Button, TextField } from '@radix-ui/themes';
 
-export default function SetBudgets() {
-    const [budgetName, setBudgetName] = useState('');
-    const [startDate, setStartDate] = useState('');
-    const [endDate, setEndDate] = useState('');
-    const [budgetAmount, setBudgetAmount] = useState('');
+const BudgetForm = () => {
+  const [budgetName, setBudgetName] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [budgetAmount, setBudgetAmount] = useState('');
 
-    const postBudgetData = async () => {
-        const budgetData = {
-          budget_id: Math.floor(Math.random() * 1000000), // Random ID for the budget
-          budget_name: budgetName,
-          start_date: startDate,
-          end_date: endDate,
-          budget_amount: parseFloat(budgetAmount),
-        };
+  const handleSetBudget = async () => {
+    const budget_id = Math.floor(Math.random() * 1000000); // Random ID for the budget
+    const budget = { budget_id, budget_name: budgetName, start_date: startDate, end_date: endDate, budget_amount: budgetAmount };
 
-        try {
-            const response = await fetch('/api/budgets', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(budgetData),
-            });
+    try {
+      const response = await fetch('/api/budgets', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(budget),
+      });
 
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            const result = await response.json();
-            alert(result.message); // Alert the success message
-            // Optionally reset form or update UI here
-            setBudgetName('');
-            setStartDate('');
-            setEndDate('');
-            setBudgetAmount('');
-        } catch (error) {
-            console.error('Failed to post budget:', error);
-        }
-    };
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const result = await response.json();
+      // Update UI as needed
+    } catch (error) {
+      console.error("Failed to post budget:", error);
+      alert("Failed to set budget.");
+    }
+  };
 
-    return (
-        <div>
-            <TextField value={budgetName} onChange={e => setBudgetName(e.target.value)} placeholder="Budget Name" />
-            <TextField type="date" value={startDate} onChange={e => setStartDate(e.target.value)} placeholder="Start Date" />
-            <TextField type="date" value={endDate} onChange={e => setEndDate(e.target.value)} placeholder="End Date" />
-            <TextField type="number" value={budgetAmount} onChange={e => setBudgetAmount(e.target.value)} placeholder="Budget Amount" />
-            <Button onClick={postBudgetData}>Add Budget</Button>
+  return (
+    <div className="flex justify-center my-5">
+      <div className="w-1/2 p-3 bg-slate-50 rounded-md">
+        <h1 className="text-xl mb-4">Set Budget</h1>
+        <div className="flex flex-col">
+          <input className="mb-2 p-2 border rounded" type="text" value={budgetName} onChange={e => setBudgetName(e.target.value)} placeholder="Budget Name" />
+          <input className="mb-2 p-2 border rounded" type="date" value={startDate} onChange={e => setStartDate(e.target.value)} placeholder="Start Date" />
+          <input className="mb-2 p-2 border rounded" type="date" value={endDate} onChange={e => setEndDate(e.target.value)} placeholder="End Date" />
+          <input className="mb-2 p-2 border rounded" type="number" value={budgetAmount} onChange={e => setBudgetAmount(e.target.value)} placeholder="Budget Amount" />
+          <button className="p-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition-colors" onClick={handleSetBudget}>Set Budget</button>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
+
+export default BudgetForm;
