df6f1cec933aabd263308f59fd577745f01aeb3b
Aaron Hsu <aaron.hsu@duke.edu>
2023-12-09 19:26:41 -0500

    transaction sum over 12 months

--- src/pages/components/transactions-page.jsx
+++ src/pages/components/transactions-page.jsx
@@ -1,6 +1,12 @@
 import { useEffect, useState } from "react";
 import TransactionsTable from "./transactions-table";
+import Histogram from "./transactions/histogram"
 
 export default function TransactionsPage({ walletId }) {
-	return <TransactionsTable walletId={walletId} />;
+	return (
+		<div>
+			<Histogram />
+			<TransactionsTable walletId={walletId} />
+		</div>
+	)
 }
---
+++ src/pages/components/transactions/histogram.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useRef, useState } from "react";
+import { Bar } from 'react-chartjs-2';
+
+export default function Histogram() {
+
+    const [payload, setPayload] = useState([]);
+    const [data, setData] = useState({
+        labels: [],
+        datasets: [
+            {
+                label: 'Spending (last 12 months)',
+                data: [],
+                backgroundColor: 'rgba(75,192,192,0.4)',
+                borderColor: 'rgba(75,192,192,1)',
+                borderWidth: 1
+            }
+        ]
+    });
+
+    const options = {
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        beginAtZero: true,
+                    },
+                },
+            ],
+        },
+    };
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/histogram")
+            .then((response) => response.json())
+            .then((payload) => setPayload(payload));
+    }, []);
+
+    useEffect(() => {
+        var data = {
+            labels: [],
+            datasets: [
+                {
+                    label: 'Spending (last 12 months)',
+                    data: [],
+                    backgroundColor: 'rgba(75,192,192,0.4)',
+                    borderColor: 'rgba(75,192,192,1)',
+                    borderWidth: 1
+                }
+            ]
+        }
+
+
+        payload.slice(0, 12).map((month, index) => {
+                
+                const monthName = getMonthNameFromYearMonth(month.month);
+                const monthAmount = month.spending;
+    
+                data.labels.push(monthName);
+                data.datasets[0].data.push(monthAmount);
+        })
+
+        data.labels.reverse()
+        data.datasets[0].data.reverse()
+
+        setData(data);
+    }, [payload])
+
+    function getMonthNameFromYearMonth(yearMonth) {
+        const [year, month] = yearMonth.split('-');
+        const date = new Date(year, month - 1);
+        return date.toLocaleString('default', { month: 'short' }) + " '" + year.slice(2);
+    }
+
+    return (
+        <>
+            <h1>Histogram</h1>
+            <button onClick={() => console.log(payload)}>Log payload</button>
+            <button onClick={() => console.log(data)}>Log data</button>
+
+            <Bar data={data} options={options} />
+        </>
+    );
+}
